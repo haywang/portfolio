075d554d75361133df19b59250910e5a52a7ba1b
“Austin” <30918019+haywang@users.noreply.github.com>
2024-12-30 21:54:11 +0800

    chatbot use its own layout

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-export const CUSTOM_LAYOUT_ROUTES = ['/ai/newchatbot']
+export const CUSTOM_LAYOUT_ROUTES = ['/ai/newchatbot', '/ai/chatbot']
 
 export const shouldUseCustomLayout = (pathname: string) => {
   return CUSTOM_LAYOUT_ROUTES.includes(pathname)
