af9c3971a7447cf476b97b4a8e287a8ae9388b70
“Austin” <30918019+haywang@users.noreply.github.com>
2025-01-05 22:17:28 +0800

    split Site component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,67 @@
 import Image from 'next/image'
 import { showcase } from '@/app/showcase'
+import clsx from 'clsx'
+// import { useRef } from 'react'
+import Link from 'next/link'
 
-// function Site({ site, priority = false }) {
-//   console.log(site, priority)
-// }
+function Site({ site, priority = false }) {
+  // let videoContentRef = useRef()
+  // let videoRef = useRef()
+  return (
+    <li className="dark:highlight-white/5 group relative rounded-3xl bg-slate-50 p-6 hover:bg-slate-100 dark:bg-slate-800/80 dark:hover:bg-slate-700/50">
+      <div className="relative aspect-[672/494] transform overflow-hidden rounded-md bg-slate-200 shadow-[0_2px_8px_rgba(15,23,42,0.08)] dark:bg-slate-700">
+        <Image
+          alt=""
+          className={clsx(
+            'absolute inset-0 h-full w-full',
+            site.dark && 'dark: hidden'
+          )}
+          src={site.thumbnail}
+          priority={priority}
+          unoptimized
+        />
+        {/*  ref={videoContentRef} ref={videoRef}*/}
+        <div>
+          <video
+            preload="none"
+            muted
+            playsInline
+            className={clsx(
+              'absolute inset-0 h-full w-full [mask-image:radial-gradient(white,black)]',
+              site.dark && 'dark:hidden'
+            )}
+          >
+            <source src={site.video} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div className="mt-6 flex flex-wrap items-center">
+        <h2 className="text-sm font-semibold leading-6 text-slate-900 group-hover:text-sky-500 dark:text-white dark:group-hover:text-sky-400">
+          <Link href={site.slug}>
+            <span className="absolute inset-0 rounded-3xl"></span>
+            {site.name}
+          </Link>
+        </h2>
+        <svg
+          className="h-6 w-6 flex-none opacity-0 group-hover:opacity-100"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M9.75 15.25L15.25 9.75M15.25 9.75H10.85M15.25 9.75V14.15"
+            stroke="#0EA5E9"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+        <p className="w-full flex-none text-[0.8125rem] leading-6 text-slate-500 dark:text-slate-400">
+          {site.description}
+        </p>
+      </div>
+    </li>
+  )
+}
 
 export default function Home() {
   const imageStyle = { color: '#ff0000' }
