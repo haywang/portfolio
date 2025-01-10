309b44a410c9d0dc822f0474333fc2d6884b54c7
“Austin” <30918019+haywang@users.noreply.github.com>
2025-01-10 20:40:39 +0800

    modify chatbot video

--- app/showcase.tsx
+++ app/showcase.tsx
@@ -3,7 +3,8 @@ export const showcase = [
     name: 'ChatBot',
     slug: 'ai/chatbot',
     thumbnail: require('@/images/showcase/thumbnails/chatbot.png').default,
-    video: '/videos/openai.com.mp4',
+    // video: '/videos/openai.com.mp4',
+    video: '/videos/chatbot.mov',
     description:
       "It's a AI chatbot useing OpenRouter and Google Gemini, meta llama 3.1 70b"
   }
